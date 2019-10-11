939f8d316aa5c080ca03fea0de276c92026e4fd6
tcheung99 <38997117+tcheung99@users.noreply.github.com>
2019-10-11 10:19:27 -0400

    Add files via upload

---
+++ candlestick2.py
@@ -0,0 +1,18 @@
+#import plotly.plotly as py
+import plotly 
+import plotly.graph_objs as go
+import json
+
+import pandas as pd
+from datetime import datetime
+
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df = pd.read_json('EUR_USD.txt')
+
+""" trace = go.Candlestick(x=df['Date'],
+                open=df['AAPL.Open'],
+                high=df['AAPL.High'],
+                low=df['AAPL.Low'],
+                close=df['AAPL.Close'])
+data = [trace]
+plotly.offline.plot(data, filename='simple_candlestick') """
